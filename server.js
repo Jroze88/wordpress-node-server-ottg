39943682c7c8cd7364f2abd2d95ae187d1cb0b5e
jroze88 <jroze88@gmail.com>
2019-11-13 15:01:21 -0800

    scraper

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-
-
-
-
+const MongoClient = require('mongodb').MongoClient;
+const cheerio = require('cheerio')
 
 
 
@@ -20,6 +18,90 @@ const path = require('path');
 const app = express();
 
 
+let dbArticles;
+let dbVideos;
+
+
+const uri = "mongodb+srv://tech-ottg-admin:ottgcodingtest123@cluster0-gdlsp.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true }, {useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+
+  dbArticles = client.db('articles');
+
+  dbVideos = client.db('videos')
+
+//   dbVideos.createCollection('video', {validator: {
+//     $jsonSchema: {
+//        bsonType: "object",
+//        required: [ "url", "title", "date", "thumb" ],
+//        properties: {
+//         url: {
+//              bsonType: "string",
+//              description: "must be a string and is required"
+//           },
+//           date: {
+//              bsonType: "date",
+//              minimum: 2017,
+//              maximum: 3017,
+//              description: "must be a date in ms and is required"
+//           },
+//           title: {
+//             bsonType: "string",
+//             description: "must be a string and is required"
+//           },
+//           thumb: {
+//             bsonType: "string",
+//             description: "must be a string and is required"
+//           }
+//        }
+//     }
+//  }
+// })
+
+
+// dbArticles.createCollection('article', {validator: {
+//   $jsonSchema: {
+//      bsonType: "object",
+//      required: [ "url", "title", "date", "thumb" ],
+//      properties: {
+//       url: {
+//            bsonType: "string",
+//            description: "must be a string and is required"
+//         },
+//         date: {
+//            bsonType: "date",
+//            description: "must be a date in ms and is required"
+//         },
+//         title: {
+//           bsonType: "string",
+//           description: "must be a string and is required"
+//         },
+//         thumb: {
+//           bsonType: "string",
+//           description: "must be a string and is required"
+//         }
+//      }
+//   }
+// }
+// })
+
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(collection)
+  }
+
+  // perform actions on the collection object
+  // client.close();
+});
+
+
+const fetchMikeMeeple = () => {
+
+  const $ = cheerio.load('<ul id="fruits">...</ul>');
+
+}
 
 
 // app.use(logger('dev'));
