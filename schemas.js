281a0284fa2d2a3ef23610fa4874205294b24b71
Jroze88 <jano.roze@gmail.com>
2019-11-21 03:48:04 -0500

    scraper works

--- schemas.js
+++ schemas.js
@@ -13,7 +13,7 @@ var contributorSchema = new Schema({
     name:  { type: String, required: true, unique: true },
     type: { type: String, required: true, unique: true },
     postNum: { type: Number, required: true},
-    lastChecked: { type: Date, default: Date.now }
+    lastChecked: { type: Date, default: Date.now },
   });
 
   var articleSchema = new Schema({
@@ -22,7 +22,9 @@ var contributorSchema = new Schema({
     date: { type: Date, default: Date.now },
     url:   { type: String, required: true, unique: true },
     thumb: { type: String, required: true },
-    author: { type: String, required: true }
+    author: { type: String, required: true },
+    thumbType: { type: String },
+    generateThumb : {type: Boolean, default: false}
   });
 
   var videoSchema = new Schema({
@@ -31,7 +33,9 @@ var contributorSchema = new Schema({
     date: { type: Date, default: Date.now },
     url:   { type: String, required: true, unique: true },
     thumb: { type: String, required: true },
-    author: { type: String, required: true }
+    author: { type: String, required: true },
+    thumbType: { type: String },
+    generateThumb : {type: Boolean, default: false}
   });
 
 
@@ -45,87 +49,6 @@ const Videos = mongoose.model('Videos', videoSchema);
 const Contributors = mongoose.model('Contributors', contributorSchema);
 
 
-
-//   dbVideos.createCollection('contributor', {validator: {
-//     $jsonSchema: {
-//        bsonType: "object",
-//        required: [ "name", "type", "lastChecked", "postNum" ],
-//        properties: {
-//         name: {
-//              bsonType: "string",
-//              description: "must be a string and is required"
-//           },
-//           lastChecked: {
-//              bsonType: "date",
-//              description: "must be a date in ms and is required"
-//           },
-//           type: {
-//             bsonType: "string",
-//             description: "must be a string and is required"
-//           },
-//           postNum: {
-//             bsonType: "number",
-//             description: "must be a number and is required"
-//           }
-//        }
-//     }
-//  }
-// })
-
-//   dbVideos.createCollection('video', {validator: {
-//     $jsonSchema: {
-//        bsonType: "object",
-//        required: [ "url", "title", "date", "thumb" ],
-//        properties: {
-//         url: {
-//              bsonType: "string",
-//              description: "must be a string and is required"
-//           },
-//           date: {
-//              bsonType: "date",
-//              minimum: 2017,
-//              maximum: 3017,
-//              description: "must be a date in ms and is required"
-//           },
-//           title: {
-//             bsonType: "string",
-//             description: "must be a string and is required"
-//           },
-//           thumb: {
-//             bsonType: "string",
-//             description: "must be a string and is required"
-//           }
-//        }
-//     }
-//  }
-// })
-
-
-// dbArticles.createCollection('article', {validator: {
-//   $jsonSchema: {
-//      bsonType: "object",
-//      required: [ "url", "title", "date", "thumb" ],
-//      properties: {
-//       url: {
-//            bsonType: "string",
-//            description: "must be a string and is required"
-//         },
-//         date: {
-//            bsonType: "date",
-//            description: "must be a date in ms and is required"
-//         },
-//         title: {
-//           bsonType: "string",
-//           description: "must be a string and is required"
-//         },
-//         thumb: {
-//           bsonType: "string",
-//           description: "must be a string and is required"
-//         }
-//      }
-//   }
-// }
-// })
 
 
 module.exports = {
--- server.js
+++ server.js
@@ -7,9 +7,9 @@
 // │    │    └─────────────── hour (0 - 23)
 // │    └──────────────────── minute (0 - 59)
 // └───────────────────────── second (0 - 59, OPTIONAL)
-
-
-const schedule = require('node-schedule')
+const secrets = require('./secrets.json')
+const Jimp = require('jimp')
+// const schedule = require('node-schedule')
 var http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -23,15 +23,290 @@ const axios = require('axios')
 const $ = require('jquery')
 const {Articles, Videos, Contributors} = require('./schemas.js')
 var randomizer = 0;
+var WPAPI = require( 'wpapi' );
+const fs = require('fs')
+const request = require('request');
+const apiRootJSON = require('./wp-json.json')
+var wp = new WPAPI({
+  endpoint: secrets.endpoint,
+  // routes: apiRootJSON,
+  // This assumes you are using basic auth, as described further below
+  username: secrets.username,
+  password: secrets.password,
+  auth: true,
+  basicAuth: {
+    username: secrets.username,
+    password: secrets.password,
+  }
+});
+
+console.log(secrets)
+
+let writeState = false
+
+let JimpGoT = Jimp.loadFont('./got.fnt') // using a callback pattern
+
+// Jimp.loadFont('./got.fnt').then(font => {
+// Jimp.read('./mikemeeple.jpg')
+//   .then(image => {
+//     image.print(
+//       font,
+//       10,
+//       10,
+//       {
+//         text: 'Hello world',
+//         alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+//         alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+//       },
+//       400, //// maxwidth
+//       380 ///// max height
+//     ); // prints 'Hello world!' on an image, middle and center-aligned, when x = 0 and y = 0
+//     image.write('./meeple.jpg')
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   });
+// }).catch(err => console.log(err))
+
+const checkThumb = function(str) {
+
+  if (str.includes('jpg')) {
+    return 'jpg'
+  }
+  if (str.includes('gif')) {
+    return 'gif'
+  }
+  if (str.includes('png')) {
+    return 'png'
+  }
+
+
+} 
+
+const download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+const contributorCategories = [
+  {'Mythicos Studios' : 162},
+  {'Chase - On The Table Gaming' : [168]},
+  {'BlitzMinis' : [163]},
+  {"Mike Meeple's Painting Poorly": [169] }
+]
+
+
+const generateThumb = [
+  {'Mythicos Studios' : false},
+  {'Chase - On The Table Gaming' : false},
+  {'BlitzMinis' : false},
+  {"Mike Meeple's Painting Poorly": true }
+]
+
+const thumbTemplate = [
+  {"Mike Meeple's Painting Poorly": './mikemeeple.jpg' }
+]
+
+
+
+
+
+
+
+const createThisVideoPost = function(postObj) {
+
+
+if (writeState === false) {
+  return true
+}
+
+download(postObj.thumb, `./${postObj.videoTitle}.${postObj.thumbType}`, function() {
+
+  if (postObj.url.includes('youtube')) {
+
+  }
+
+
+  wp.posts().create({
+    "type": "post",
+    "title":  postObj.videoTitle,
+    "content": `<span class='redirect-this-custom-scraper' id='${postObj.url}'></span>`,
+    "status": "publish",
+    "author": 6,
+    "format": "standard",  
+    "tags" : [165, 166, 167],
+    "categories" : [contributorCategories[postObj.author]]
+  }).then(function( response ) {
+    // "response" will hold all properties of your newly-created post,
+    // including the unique `id` the post was assigned on creation
+    console.log( response.id);
+    let thisID = response.id
+    wp.media().file( `./${postObj.videoTitle}.${postObj.thumbType}`).create({
+      title: 'Amazing featured image',
+      post: thisID,
+      "media[0]" : `${postObj.thumb}`
+    }).then(function( media ) {
+      console.log(media)
+      return wp.posts().id(thisID).update({
+        featured_media: media.id
+      })
+
+    }).catch(err => console.log(err))
+    
+}).catch(err => console.log(err))
+
+
+})
+
+
+
+}
+
+
+
+
+const createThisVideoPostWithThumb = function(postObj) {
+
+  if (writeState === false) {
+    return true
+  }
+ 
+    Jimp.read(`./${thumbTemplate[postObj.author]}`)
+      .then(image => {
+        image.print(
+          JimpGoT,
+          10,
+          10,
+          {
+            text: `${postObj.title}`
+          },
+          400, //// maxwidth
+          380 ///// max height
+        ); // prints 'Hello world!' on an image, middle and center-aligned, when x = 0 and y = 0
+      }).then(function() {
+
+        wp.posts().create({
+          "type": "post",
+          "title":  postObj.videoTitle,
+          "content": `<span class='redirect-this-custom-scraper' id='${postObj.url}'></span>`,
+          "status": "publish",
+          "author": 6,
+          "format": "standard",  
+          "tags" : [165, 166, 167],
+          "categories" : [contributorCategories[postObj.author]]
+        }).then(function( response ) {
+          // "response" will hold all properties of your newly-created post,
+          // including the unique `id` the post was assigned on creation
+          console.log( response.id);
+          let thisID = response.id
+          wp.media().file( `${thumbTemplate[postObj.author]}new`).create({
+            title: 'Amazing featured image',
+            post: thisID,
+            "media[0]" : `${thumbTemplate[postObj.author]}new`
+          }).then(function( media ) {
+            console.log(media)
+            return wp.posts().id(thisID).update({
+              featured_media: media.id
+            })
+      
+          }).catch(err => console.log(err))
+          
+     
+      
+      
+      })
+
+      })
+      .catch(err => {
+        console.log(err)
+      });
+   
+
+ 
+  
+  
+  
+  
+
+  
+  
+  
+  }
+
+
+// const createThisMeeplePost = function(postObj) {
+
+
+
+
+//   download(postObj.thumb, `./${postObj.articleTitle}.${postObj.thumbType}`, function() {
+  
+//     wp.posts().create({
+//       "type": "post",
+//       "title":  postObj.articleTitle,
+//       "content": `<span class='redirect-this-custom-scraper' id='${postObj.url}'></span>`,
+//       "status": "publish",
+//       "author": 6,
+//       "format": "standard",
+//       "tags" : [165, 166, 167],
+//       "categories" : [169]
+//     }).then(function( response ) {
+//       // "response" will hold all properties of your newly-created post,
+//       // including the unique `id` the post was assigned on creation
+//       console.log( response.id);
+//       let thisID = response.id
+//       wp.media().file( `./${postObj.articleTitle}.${postObj.thumbType}`).create({
+//         title: 'Amazing featured image',
+//         post: thisID,
+//         "media[0]" : `${postObj.thumb}`
+//       }).then(function( media ) {
+//         console.log(media)
+//         return wp.posts().id(thisID).update({
+//           featured_media: media.id
+//         })
+  
+//       }).catch(err => console.log(err))
+      
+//   }).catch(err => console.log(err))
+  
+  
+//   })
+  
+  
+  
+//   }
+
+
+
+  
+  
+ 
+// mongoose.connection.close()
+mongoose.connect(secrets.uri2, {useNewUrlParser: true }).catch(error => console.log(error));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+  
+  console.log('Mongo Connection Secured')
+
+
+
+});
+
+
+
 
 
 
 
 const app = express();
 
-const uri1 = "mongodb+srv://tech-ottg-admin:ottgcodingtest123@cluster0-gdlsp.gcp.mongodb.net/Articles?retryWrites=true&w=majority";
-const uri2 = "mongodb+srv://tech-ottg-admin:ottgcodingtest123@cluster0-gdlsp.gcp.mongodb.net/Videos?retryWrites=true&w=majority";
-// const uri3 = "mongodb+srv://tech-ottg-admin:ottgcodingtest123@cluster0-gdlsp.gcp.mongodb.net/Contributors?retryWrites=true&w=majority";
+
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -44,21 +319,18 @@ app.use(cookieParser());
 
 
 
+const server = http.createServer(app)
+
+server.listen();
+
+
+
+
+
+
+
+
 const meepleServerCheck = function() {
-  mongoose.connection.close()
-mongoose.connect(uri1, {useNewUrlParser: true, useUnifiedTopology: true }).catch(error => console.log(error));
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-
-  
-  console.log('Mongo Connection Secured')
-  fetchMikeMeeple()
-});
-
-
-
-
 
 
 
@@ -70,7 +342,7 @@ const fetchMikeMeeple = () => {
 
   
 
-  axios.get('https://mikemeeple.blogspot.com/p/blog-page_16.html').then(response => {
+  axios.get(secrets.MikeMeeple).then(response => {
 
 
     meepleArchive = response.data
@@ -86,24 +358,33 @@ const fetchMikeMeeple = () => {
       if ($(el).attr('href').includes('meeple')) {
 
         let thisTimeStamp = Date.now()
-        let newItem = new Articles({
+        let thisItem = new Articles({
           articleTitle: `Mike Meeple's ASOIAF-Painting-Poorly: ${$(el).text()}`,
           url: $(el).attr('href'),
           thumb: 'https://4.bp.blogspot.com/-PfpKrMXj1Ek/WqLncktF6UI/AAAAAAAAAFg/VbbdD_UqbowDMg7aG4tobXVnSz5liRiEACK4BGAYYCw/s113-pf/MikeMeepleTransparent.png',
           date: thisTimeStamp,
-          author: 'Mike Meeple'
+          author: 'Mike Meeple',
+          thumbType: 'png'
         })
 
-        newItem.save().catch((error, saved) => {
-          if (error) {
-            console.log(error)
-          } else {
-            ///////////////////////////////////////////////////////////////////////////////// WORDPRESSPOST
-          }
-        })
-      } else {
-        return
+        if (thisItem.articleTitle.includes('ASOIAF'))  {
+
+
+          thisItem.save().then((saved, err) => {
+         if (saved) {
+          // createThisVideoPostWithThumb(thisItem)
+         }
+       }).catch(err => console.log(err))
+     } else if (thisItem.articleTitle.includes('Song of Ice'))  {
+ 
+ 
+       thisItem.save().then((saved, err) => {
+      if (saved) {
+        // createThisVideoPostWithThumb(thisItem)
       }
+    }).catch(err => console.log(err))
+  }
+      } 
       
    })
 
@@ -112,39 +393,16 @@ const fetchMikeMeeple = () => {
     console.log(error);
   })
 
-// const bulkOp = Articles.initializeOrderedBulkOp();
-
-
-// meepleArticles.forEach(element => {
-//     bulkOp.insert(element)
-// });
-
-// // Adds three inserts to the bulkOp.
-// // bulkOp
-// //   .insert({ a: 1 })
-// //   .insert({ b: 2 })
-// //   .insert({ c: 3 });
-// bulkOp.execute().then((succ) => {
-//   console.log(succ)
-// }).catch(err => console.log(err))
-
-
 
 
 }
 
+fetchMikeMeeple()
 
 }
-
-
-const youTubeServerCheck = function(url, aut) {
-
-  let givenUrl = url;
-  let Author = aut
 
   const fetchVideos = function(givenUrl, Author) {
 
-    // let testUrl = 'https://www.youtube.com/channel/UCfmdCCJ9Usa686sLnlnVN6Q/videos?view=0&sort=dd&flow=grid'
   
     axios.get(givenUrl).then(response => {
   
@@ -167,20 +425,38 @@ const youTubeServerCheck = function(url, aut) {
 
     let item = $('h3').children('a').map((i, el) => {
       // console.log($(el).attr('href'))
+
+      
+
+
       let thisItem = new Videos({
         videoTitle: $(el).text(),
         url: `http://www.youtube.com${$(el).attr('href')}`,
-        thumb: $(el).parent().parent().parent().parent().find('img').attr('src'),
-        author: Author
+        thumb: $(el).parent().parent().parent().parent().find('img').attr('src').split("\?")[0],
+        author: Author,
+        thumbType: checkThumb($(el).parent().parent().parent().parent().find('img').attr('src').split("\?")[0])
       })
 
-      thisItem.save().catch((error, saved) => {
-        if (error) {
-          console.log(error)
-        } else {
-          ///////////////////////////////////////////////////////////////////////////////// WORDPRESSPOST
+      console.log(thisItem)
+      
+
+        if (thisItem.videoTitle.includes('ASOIAF'))  {
+
+
+         thisItem.save().then((saved, err) => {
+        if (saved) {
+          // createThisVideoPost(thisItem)
         }
-      })
+      }).catch(err => console.log(err))
+    } else if (thisItem.videoTitle.includes('Song of Ice'))  {
+
+
+      thisItem.save().then((saved, err) => {
+     if (saved) {
+      //  createThisVideoPost(thisItem)
+     }
+   }).catch(err => console.log(err))
+ }
 
 
     })
@@ -189,64 +465,43 @@ const youTubeServerCheck = function(url, aut) {
   })
 
 
-}
-  
-mongoose.connection.close()
-mongoose.connect(uri2, {useNewUrlParser: true, useUnifiedTopology: true }).catch(error => console.log(error));
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-
-  
-  console.log('Mongo Connection Secured')
-  fetchVideos(givenUrl, Author)
-});
-
-
-
-
-}
-
-
-let youTubeOttg = 'https://www.youtube.com/channel/UCfmdCCJ9Usa686sLnlnVN6Q/videos'
-let blitzMinis = 'https://www.youtube.com/channel/UCshI4GSr6P6tzzQdGuc3dMQ/search?query=ice+and+fire'
-let mythicosStudios = 'https://www.youtube.com/channel/UCTTKb6Fu1WOdkdGzq8c5hiw/videos'
-
-
-youTubeServerCheck(youTubeOttg, 'Chase - On the Table Gaming')
-
-
-setTimeout(()=> {
-  meepleServerCheck()
-}, 15000)
-
-setTimeout(()=> {
-  youTubeServerCheck(blitzMinis, 'BlitzMinis')
-}, 30000)
-
-
-setTimeout(()=> {
-  youTubeServerCheck(mythicosStudios, 'Mythicos Studios')
-}, 45000)
+  }
 
 
 
 
 
-setInterval(() => {
-  meepleServerCheck()
-}, 34000000 + randomizer * 42038)
+
+
+
+
+
+
+
+
+// fetchVideos(secrets.youTubeOttg, 'Chase - On the Table Gaming')
+
+
+// setTimeout(()=> {
+//   fetchVideos(secrets.mythicosStudios, 'Mythicos Studios')
+// }, 15000)
+
+// setTimeout(()=> {
+//   fetchVideos(secrets.blitzMinis, 'BlitzMinis')
+// }, 30000)
+
+
+// setTimeout(()=> {
+//   meepleServerCheck()
+// }, 45000)
+
+
+
+
+
+// setInterval(() => {
+//   meepleServerCheck()
+// }, 34000000 + randomizer * 42038)
 
 // meepleServerCheck()
 // youTubeServerCheck(uri2, 'author')
-
-
-app.get('*', (req, res) => {
-  res.json('Hello!')
-});
-
-
-
-const server = http.createServer(app)
-
-server.listen();
